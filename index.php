6f87ead9b209f15aaffdc3c348abafb7468562c9
Tony <tony.g.thompson@gmail.com>
2018-02-20 16:46:31 -0800

    reorganized layout

--- index.php
+++ index.php
@@ -1,20 +1,42 @@
 <?php
 
+//array to test against
+$parts = array("AP-12-3507", "  ap-99-X109  ", "SG-05-ab20", "ab-22-N250", "SG-xx-N250", "SG-22-250", "SG-22-250*");
+
+
+/***** PART 2 *****/
+//tests regular expressions
+foreach ($parts as $part) {
+    if (validPartRegEx($part)) echo "$part is valid.<br>";
+    else  echo "$part is not valid.<br>";
+}
+
 function validPartRegEx($str)
 {
     //For Micheal
+    return true;
+}
+
+
+/***** PART 1 *****/
+//tests built in php function method
+foreach ($parts as $part) {
+    if (validPartFunction($part)) echo "$part is valid.<br>";
+    else  echo "$part is not valid.<br>";
 }
 
 function validPartFunction($str)
 {
-    if ($str == null) return false;
-    $bits = explode('-', $str);
-    if (sizeof($bits) != 3) return false;
+    if ($str == null) return false;  //check if null
+    $trim = trim($str);  //removes blank space at start and end
+    $bits = explode('-', $trim);  //splits string into string array at each -
+    if (sizeof($bits) != 3) return false;  //if incorrect number of segments
+    //puts array parts into separate variables
     $cat = $bits[0];
     $ware = $bits[1];
     $part = $bits[2];
 
-    $valid = true;
+
     $valid = validCat($cat);
     $valid = validWare($ware);
     $valid = validPart($part);
@@ -40,13 +62,3 @@ function validPart($part)
 }
 
 
-$parts = array("AP-12-3507", "  ap-99-X109  ", "SG-05-ab20", "ab-22-N250", "SG-xx-N250", "SG-22-250", "SG-22-250*");
-foreach ($parts as $part) {
-    if (validPartFunction($part)) echo "$part is valid.<br>";
-    else  echo "$part is not valid.<br>";
-}
-
-foreach ($parts as $part) {
-    if (validPartRegEx($part)) echo "$part is valid.<br>";
-    else  echo "$part is not valid.<br>";
-}
