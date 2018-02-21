3fb4486ec983ba3e14c75b245c81d2098e280191
Tony <tony.g.thompson@gmail.com>
2018-02-20 17:20:56 -0800

    Part 1 finished

--- index.php
+++ index.php
@@ -1,24 +1,18 @@
 <?php
 
+/*
+ * Tony Thompson & Micheal Horn
+ * 2/20/18
+ * http://athompson.greenriverdev.com/328/pairprogram/data-validation/
+ * tests validity of given part numbers with regular expressions and without
+ */
+
 //array to test against
 $parts = array("AP-12-3507", "  ap-99-X109  ", "SG-05-ab20", "ab-22-N250", "SG-xx-N250", "SG-22-250", "SG-22-250*");
 
 
-/***** PART 2 *****/
-//tests regular expressions
-foreach ($parts as $part) {
-    if (validPartRegEx($part)) echo "$part is valid.<br>";
-    else  echo "$part is not valid.<br>";
-}
-
-function validPartRegEx($str)
-{
-    //For Micheal
-    return true;
-}
-
-
 /***** PART 1 *****/
+echo "<h4> Part 1</h4>";
 //tests built in php function method
 foreach ($parts as $part) {
     if (validPartFunction($part)) echo "$part is valid.<br>";
@@ -33,35 +27,36 @@ function validPartFunction($str)
     if (sizeof($bits) != 3) return false;  //if incorrect number of segments
 
     $valid = true;
-
-        //check department category
+    //check department category
     $cat = strtoupper($bits[0]);
-    if (!($cat == "HW" || $cat == "SG" || $cat == "AP") && (strlen($cat) == 2)) {
+    if (!(($cat == "HW" || $cat == "SG" || $cat == "AP") && (strlen($cat) == 2))) {
         $valid = false;
     }
-
     //check warehouse number
-    if (!ctype_digit($bits[1]) && (strlen($bits[1]) == 2)) {
+    if (!(ctype_digit($bits[1]) && (strlen($bits[1]) == 2))) {
         $valid = false;
     }
-
     //check part number
-    if (!ctype_alnum($bits[2]) && (strlen($bits[2]) == 4)) {
+    if (!(ctype_alnum($bits[2]) && (strlen($bits[2]) == 4))) {
         $valid = false;
     }
-
     return $valid;
 }
 
 
+/***** PART 2 *****/
+echo "<h4> Part 2</h4>";
+//tests regular expressions
+foreach ($parts as $part) {
+    if (validPartRegEx($part)) echo "$part is valid.<br>";
+    else  echo "$part is not valid.<br>";
+}
 
-
-
-
-
-
-
-
+function validPartRegEx($str)
+{
+    //For Micheal
+    return true;
+}
 
 
 
